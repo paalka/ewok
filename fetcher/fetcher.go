f8265ade5f172735dc36c42b6ec2bbca861ee934
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-05-23 23:42:15 +0200

    Create a simple script for loading RSS feeds.

---
+++ fetcher/fetcher.go
@@ -0,0 +1,25 @@
+package fetcher
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+)
+
+type Feed struct {
+	LastPublished string
+	FeedUrl       string
+}
+
+func ReadFeed(feed Feed, fp *gofeed.Parser, ch chan Feed, chFinished chan bool) {
+	rssFeed, err := fp.ParseURL(feed.FeedUrl)
+
+	if err != nil {
+		fmt.Printf("%s %s", err, feed.FeedUrl)
+	}
+
+	if rssFeed != nil {
+		ch <- Feed{LastPublished: rssFeed.Items[0].Published, FeedUrl: feed.FeedUrl}
+	}
+
+	chFinished <- true
+}
