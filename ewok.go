2d7b9c85658ef6fa8b9a406de177fd969466cebc
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-03 17:38:24 +0200

    Add a handler which can be used to display paginated sites.

--- ewok.go
+++ ewok.go
@@ -8,6 +8,11 @@ import (
 	"github.com/paalka/ewok/feed"
 	"html/template"
 	"net/http"
+	"strconv"
+)
+
+const (
+	ITEMS_PER_PAGE = 10
 )
 
 func renderTemplate(w http.ResponseWriter, templates *template.Template, tmpl_name string, data interface{}) {
@@ -30,12 +35,38 @@ func makeIndexHandler(config config.Config, templates *template.Template) http.H
 	}
 }
 
+func makePageHandler(config config.Config, templates *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		db := db.GetDatabaseConnection(config.DB_NAME, config.DB_USER, config.DB_PASS)
+		possibleIndex := r.URL.Path[(len("/page/")):]
+
+		if _, err := strconv.Atoi(possibleIndex); err != nil {
+			http.Error(w, "Page not found!", http.StatusNotFound)
+			return
+		}
+
+		index, err := strconv.ParseUint(possibleIndex, 10, 64)
+
+		if err != nil {
+			http.Error(w, "Page not found!", http.StatusNotFound)
+			return
+		}
+		feedItems := feed.GetPaginatedFeeds(db, ITEMS_PER_PAGE, uint(index))
+		feeds := feed.GetFeeds(db)
+		renderTemplate(w, templates, "index", struct {
+			FeedItems []feed.EwokItem
+			Feeds     []feed.EwokFeed
+		}{feedItems, feeds})
+	}
+}
+
 func main() {
 	portPtr := flag.Int("port", 8080, "The port to use when running the server")
 	flag.Parse()
 	config := config.LoadJsonConfig("config.json")
 	templates := template.Must(template.ParseFiles("web/templates/index.html"))
 
+	http.HandleFunc("/page/", makePageHandler(config, templates))
 	http.HandleFunc("/", makeIndexHandler(config, templates))
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nil)
 	if err != nil {
