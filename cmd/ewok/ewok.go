974a40381199da9a0a817dd7a06174684fb8429c
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-07-02 11:19:54 +0200

    Move the fileserver to a dedicated file, as it is only used during development anyways.

--- cmd/ewok/ewok.go
+++ cmd/ewok/ewok.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -89,24 +88,6 @@ func makePageHandler(config config.Config, templates *template.Template, db *sql
 			Feeds     []feed.EwokFeed
 		}{feedItems, feeds})
 	}
-}
-
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit URL parameters.")
-	}
-
-	fs := http.StripPrefix(path, http.FileServer(root))
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fs.ServeHTTP(w, r)
-	}))
 }
 
 func main() {
